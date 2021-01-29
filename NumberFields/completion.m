2157491b49a0cdfaf34bd302b835c813d22ba57b
Casper Putz <casper.putz@gmail.com>
2021-01-29 11:55:22 +0100

    allow number field completion to use a database with local fields

--- NumberFields/completion.m
+++ NumberFields/completion.m
@@ -1,7 +1,9 @@
 
-intrinsic Completion(K::FldNum[FldRat], p::RngIntElt) -> EtAlg
+intrinsic Completion(K::FldNum[FldRat], p::RngIntElt
+    : D := LocalFieldDatabase()
+) -> EtAlg
 {Returns the completion of K at p.}
 	Qp := pAdicField(p,500);
 	Rp := PolynomialRing(Qp);
-	return EtaleAlgebra(Rp!DefiningPolynomial(K));
+	return EtaleAlgebra(Rp!DefiningPolynomial(K) : D := D);
 end intrinsic;
