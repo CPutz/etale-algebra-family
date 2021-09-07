1451e9f9a4cb79e084802e4d0f0f02a9cb762baa
Unknown <css_mn@yahoo.ie>
2021-09-07 16:28:05 +0200

    polynomial for monogenicty criterion

--- NumberFields/minimal.m
+++ NumberFields/minimal.m
@@ -1,4 +1,5 @@
 Q := Rationals();
+Z := Integers();
 
 intrinsic GeneralMinimalPolynomial(K::FldNum) -> RngUPolElt
 {}
@@ -9,4 +10,23 @@ intrinsic GeneralMinimalPolynomial(K::FldNum) -> RngUPolElt
 	M := IdentityMatrix(S,Degree(K)) * x - Mrep;
 
 	return Determinant(M);
+end intrinsic;
+
+intrinsic MonogenicityPolynomial(K::FldNum) -> RngUPolElt
+{}
+	R<[A]> := PolynomialRing(K,Degree(K));
+	S := PolynomialRing(Z,Degree(K));
+	OK := Integers(K);
+	B := IntegralBasis(K);
+	x := &+[A[i] * B[i] : i in [1..Degree(K)]];
+
+	M := [];
+	for j in [1..Degree(K)] do
+		Cs, Ms := CoefficientsAndMonomials((x - A[1])^j);
+		Cs2 := [[R!c*Ms[i] : c in Eltseq(OK!Cs[i])] : i in [1..#Cs]];
+		row := [&+[Cs2[k,i] : k in [1..#Cs2]] : i in [1..Degree(K)]];
+		Append(~M, row);
+	end for;
+
+	return Determinant(MatrixAlgebra(S,Degree(K))!M);
 end intrinsic;
