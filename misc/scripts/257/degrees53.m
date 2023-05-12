725b589c583e9712cf61465b7d313fb9b1a1d487
Casper Putz <casper.putz@gmail.com>
2023-05-12 12:33:28 +0200

    degrees (5,3)

--- misc/scripts/257/degrees53.m
+++ misc/scripts/257/degrees53.m
@@ -1,6 +1,8 @@
 // Load this file from the main folder
 AttachSpec("spec");
 
+Q := Rationals();
+
 function contains_components_isomorphic_to(E1,E2);
 	C1 := Components(E1);
 	C2 := Components(E2);
@@ -76,55 +78,40 @@ assert #L3_filter eq 1;
 assert IsIsomorphic(L3_filter[1], M);
 L := SplittingField(L3_filter[1]);
 
-printf "Computing a full-rank subgroup of the Mordell-Weil group of E1 over %o\n", L;
+printf "Computing a full-rank subgroup of the Mordell-Weil group of E over %o\n", L;
 
-E1L := BaseChange(E1,L);
-_,G,GtoE1L := PseudoMordellWeilGroup(E1L);
+EL := BaseChange(E,L);
+_,G,GtoEL := PseudoMordellWeilGroup(EL);
 
-//rankbound := RankBound(E1L);
+//rankbound := RankBound(EL);
 rankbound := 2;
 assert rankbound eq TorsionFreeRank(G);
 
-print "Found full-rank subgroup of E1(L) (rank = %o)\n", TorsionFreeRank(G);
+printf "Found full-rank subgroup of E(L) (rank = %o)\n", TorsionFreeRank(G);
 
-gens := [GtoE1L(g) : g in Generators(G)];
-sat23 := Saturation(Saturation([E1L!p : p in gens], 2), 3);
+gens := [GtoEL(g) : g in Generators(G)];
+sat23 := Saturation(Saturation([EL!p : p in gens], 2), 3);
 A := AbelianGroup([Order(g) : g in sat23]);
-AtoE1L := map< A -> E1L | x :-> &+[Eltseq(x)[i]*sat23[i] : i in [1..#sat23]] >;
+AtoEL := map< A -> EL | x :-> &+[Eltseq(x)[i]*sat23[i] : i in [1..#sat23]] >;
 
 printf "Constructed subgroup which is saturated at 2 and 3.\n";
 
 printf "Performing elliptic curve Chabauty\n";
 
-PhiE1L := map< E1L -> P1 | DefiningEquations(PhiE1) >;
+PhiEL := map< EL -> P1 | DefiningEquations(PhiE) >;
 
-time V,R := Chabauty(AtoE1L, PhiE1L : IndexBound := 6);
+time V,R := Chabauty(AtoEL, PhiEL : IndexBound := 6);
 assert R lt Infinity();
 assert PrimeDivisors(R) subset {2,3};
 
 
-pts_E1L := { AtoE1L(v) : v in V };
-printf "The points in E1(L) with rational image under Φ_E1 are %o\n", pts_E1L;
+pts_EL := { AtoEL(v) : v in V };
+printf "The points in E(L) with rational image under Φ_E are %o\n", pts_EL;
 
-CL := BaseChange(C,L);
-CLtoE1L := map< CL -> E1L | DefiningEquations(CtoCsigma * CsigmatoE0 * E0toE1) >;
+assert forall { p : p in pts_EL |
+	(p[1] in Q and p[2] in Q and p[3] in Q) or
+	PhiEL(p) eq P1![0,1] };
 
-pts_CL := {@ @};
-for p in pts_E1L do
-	S := Cluster(E1L, p);
-	pts_CL join:= RationalPoints(Pullback(CLtoE1L, S));
-end for;
-
-printf "The points in C(L) with rational image under ϕ are %o\n", pts_CL;
-
-F := x * (25*x^3 + 20*x^2 + 14*x + 14);
-assert forall { p : p in pts_CL | p[3] eq 0 or (Evaluate(F,p[1]) eq 0 and Evaluate(F,p[2]) eq 0 and p[3] eq 1) };
-
-printf "\nThe coordinates all these points are roots of X * (25X^3 + 20X^2 + 14X + 14).\n";
-
-assert forall { p : p in pts_CL | p[3] eq 0 or Phi(C(L)!Eltseq(p)) eq P1![0,1] };
-
-printf "All points (except for the points at infinity) map to (0 : 1) under ϕ.\n";
-
+printf "All points of E(L) - E(Q) map to (0 : 1) under Φ_E.\n";
 
 printf "\ndone\n";
