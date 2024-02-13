ad365305c9db1763d1d30acb1b252d3f349101a2
Casper Putz <casper.putz@gmail.com>
2024-02-13 10:41:20 +0100

    update script with new parameter space implementation

--- examples/ex_257.m
+++ examples/ex_257.m
@@ -4,8 +4,8 @@ AttachSpec("spec");
 R<s> := PolynomialRing(Rationals());
 _<t> := PolynomialRing(R);
 F1 := (10*t^4 + 4*t^3 + 2*t^2 + 2*t - 1)^2 - s*(4*t - 1);
-time E := EtaleAlgebraFamily(F1, 5 : MinVal := 2,
-	CongrVal := Integers(2)!0, Precision := 200);
+A1 := pAdicNbhdSpace(Rationals(), 5 : MinVal := 2, CongrVal := Integers(2)!0);
+time E := EtaleAlgebraFamily(F1, 5 : ParameterSpace := A1, Precision := 200);
 E;
 
 ClearData(~E[1]);
--- scripts/3511/local_algebras.m
+++ scripts/3511/local_algebras.m
@@ -14,11 +14,24 @@ printf "==================================================================\n\n";
 printf "------------------------------------------------------------------\n";
 printf "performing computations for the prime 2\n";
 printf "------------------------------------------------------------------\n\n";
-E2 := EtaleAlgebras3511Coeff(2, 1, 1, 1);
-
 Q2 := pAdicField(2,500);
 U2 := [EtaleAlgebra([UnramifiedExtension(Q2,n) : n in e], Q2) : e in [[10,1], [5,3,2,1], [4,4,2,1]]];
+
+E2 := EtaleAlgebras3511Coeff(2, 1, 1, 1);
 
 assert valid_covering_set(U2, E2); //sufficient
 assert valid_covering_set(E2, U2); //necessary
 printf "valid and minimal covering set the prime 2\n\n";
+
+printf "\n==================================================================\n";
+printf "We perform the computations for Proposition 6.12. \n";
+printf "==================================================================\n\n";
+
+printf "------------------------------------------------------------------\n";
+printf "performing computations for the prime 3\n";
+printf "------------------------------------------------------------------\n\n";
+
+Q3 := pAdicField(3,500);
+
+printf "case v3(a) = 4\n";
+U3_a34 := 
